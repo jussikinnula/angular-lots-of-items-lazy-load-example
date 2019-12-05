e6d4abc0a5c537be36ea45f696095f01426fd790
Jussi Kinnula <jussi.kinnula@efecte.com>
2019-12-05 09:06:43 +0200

    Add queued and loading states to ChuckNorrisService, and show the status of loading in UI

--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -10,12 +10,15 @@ import { ChuckNorrisService } from '../../services/chuck-norris.service';
 export class ItemComponent implements OnInit {
   @Input() id: number;
   content: Observable<string>;
+  queued: Observable<boolean>;
+  loading: Observable<boolean>;
 
   constructor(private chuckNorrisService: ChuckNorrisService) {}
 
   ngOnInit() {
     this.content = this.chuckNorrisService.getContent(this.id);
-
+    this.queued = this.chuckNorrisService.getQueueState(this.id);
+    this.loading = this.chuckNorrisService.getLoadingState(this.id);
   }
 
   loadContent(itemIsInViewport: boolean) {
--- src/app/services/chuck-norris.service.ts
+++ src/app/services/chuck-norris.service.ts
@@ -8,6 +8,14 @@ interface Content {
   [key: string]: string;
 }
 
+interface QueueState {
+  [key: string]: boolean;
+}
+
+interface LoadingState {
+  [key: string]: boolean;
+}
+
 interface ChuckNorrisResponse {
   id: string;
   value: string;
@@ -20,11 +28,21 @@ export class ChuckNorrisService {
   private content: Content = {};
   private contentSubject: BehaviorSubject<Content>;
   private contentObservable: Observable<Content>;
+  private queueState: QueueState = {};
+  private queueStateSubject: BehaviorSubject<QueueState>;
+  private queueStateObservable: Observable<QueueState>;
+  private loadingState: LoadingState = {};
+  private loadingStateSubject: BehaviorSubject<LoadingState>;
+  private loadingStateObservable: Observable<LoadingState>;
   private limit = pLimit(3);
 
   constructor(private http: HttpClient) {
     this.contentSubject = new BehaviorSubject(this.content);
     this.contentObservable = this.contentSubject.asObservable();
+    this.queueStateSubject = new BehaviorSubject(this.queueState);
+    this.queueStateObservable = this.queueStateSubject.asObservable();
+    this.loadingStateSubject = new BehaviorSubject(this.loadingState);
+    this.loadingStateObservable = this.loadingStateSubject.asObservable();
   }
 
   getContent(id: number): Observable<string> {
@@ -35,14 +53,67 @@ export class ChuckNorrisService {
       );
   }
 
-  async loadContent(id: number): Promise<string> {
-    const res = await this.limit(() => this.apiRequest().toPromise());
-    this.content[id] = res.body.value;
+  getQueueState(id: number): Observable<boolean> {
+    return this.queueStateObservable
+      .pipe(
+        map(queueState => queueState[id] || false),
+        distinctUntilChanged()
+      );
+  }
+
+  getLoadingState(id: number): Observable<boolean> {
+    return this.loadingStateObservable
+      .pipe(
+        map(loadingState => loadingState[id] || false),
+        distinctUntilChanged()
+      );
+  }
+
+  async loadContent(id: number): Promise<void> {
+    // In the beginning mark the card being queued, and unset previous content
+    this.setQueueState(id, true);
+    this.setLoadingState(id, false);
+    this.setContent(id, undefined);
+
+    // This delay is to see "loading queued" text in UI
+    await this.delay();
+
+    const res = await this.limit(() => {
+      // When request will be actually started, set loading state and unset queued state
+      this.setQueueState(id, false);
+      this.setLoadingState(id, true);
+      return this.apiRequest().toPromise();
+    });
+
+    // This delay is to see "loading started" text in UI
+    await this.delay();
+
+    // After loading is finished reset both states and set content
+    this.setQueueState(id, false);
+    this.setLoadingState(id, false);
+    this.setContent(id, res.body.value);
+  }
+
+  private setContent(id: number, value: string) {
+    this.content[id] = value;
     this.contentSubject.next(this.content);
-    return this.content[id];
+  }
+
+  private setQueueState(id: number, value: boolean) {
+    this.queueState[id] = value;
+    this.queueStateSubject.next(this.queueState);
+  }
+
+  private setLoadingState(id: number, value: boolean) {
+    this.loadingState[id] = value;
+    this.loadingStateSubject.next(this.loadingState);
   }
 
   private apiRequest(): Observable<HttpResponse<ChuckNorrisResponse>> {
     return this.http.get<ChuckNorrisResponse>('https://api.chucknorris.io/jokes/random', { observe: 'response' });
+  }
+
+  private delay(timeout: number = 1000): Promise<void> {
+    return new Promise(resolve => setTimeout(() => resolve(), timeout));
   }
 }
